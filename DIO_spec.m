0bb5fe7a93fd9bddc1805234c6cc0e13b42ee193
szafron@github.com <robert.szafron@gmail.com>
2015-07-30 15:23:38 -0600

    first results

--- DIO_spec.m
+++ DIO_spec.m
@@ -1,8 +1,8 @@
 (* ::Package:: *)
 
-Zal=10/137;
+Zal=10./137.;
 y=Zal;
-m=1;
+m=1.;
 NumBR=15;
 BohrRadius=1/(m*Zal);
 Efm=m (Sqrt[1-Zal^2]);
@@ -19,9 +19,12 @@ ge[r_,kappa_,Ee_]:=Evaluate[u1Electron[r,kappa,Ee]/r];
 fe[r_,kappa_,Ee_]:=Evaluate[u2Electron[r,kappa,Ee]/r];
 geI[k_,Ee_]:=geI[k,Ee]=Evaluate[Interpolation[Evaluate[Table[{r,ge[r,k,Ee]},{r,10^-6,NumBR BohrRadius,0.05}]]]];
 feI[k_,Ee_]:=feI[k,Ee]=Evaluate[Interpolation[Evaluate[Table[{r,fe[r,k,Ee]},{r,10^-6,NumBR BohrRadius,0.05}]]]];
+
 S0[k_?NumericQ,kappa_?NumericQ,J_?NumericQ,Ee_?NumericQ]:=Module[{jk=Abs[kappa]-1/2,lk},
+Print["Calculating S0"];
 lk=jk+1/2 kappa/Abs[kappa];1/2*NIntegrate[r^2 SphericalBesselJ[J,k r] ((1+(-1)^(lk+J)) (1+kappa) (geI[kappa,Ee][r] G[r]-feI[kappa,Ee][r] F[r])+I (1-(-1)^(lk+J)) (1-kappa) (feI[kappa,Ee][r] G[r]+geI[kappa,Ee][r] F[r])),{r,rinit,NumBR BohrRadius}]];
 S1[k_?NumericQ,kappa_?NumericQ,J_?NumericQ,Ee_?NumericQ]:=Module[{jk=Abs[kappa]-1/2,lk},
+Print["Calculating S1"];
 lk=jk+1/2 kappa/Abs[kappa];1/2*NIntegrate[r^2 SphericalBesselJ[J+1,k r] ((1+(-1)^(lk+J)) ((J+kappa+2) geI[kappa,Ee][r] F[r]+(kappa-J) feI[kappa,Ee][r] G[r])+I (1-(-1)^(lk+J)) ((J-kappa+2) feI[kappa,Ee][r] F[r]+(J+kappa) geI[kappa,Ee][r] G[r])),{r,rinit,NumBR BohrRadius}]];
 Sm1[k_?NumericQ,kappa_?NumericQ,J_?NumericQ,Ee_?NumericQ]:=Module[{jk=Abs[kappa]-1/2,lk},
 lk=jk+1/2 kappa/Abs[kappa];1/2*NIntegrate[r^2 SphericalBesselJ[J-1,k r] ((1+(-1)^(lk+J)) ((1+kappa-J) geI[kappa,Ee][r] F[r]+(J+kappa+1) feI[kappa,Ee][r] G[r])+I (1-(-1)^(lk+J)) ((1-kappa-J) feI[kappa,Ee][r] F[r]-(J-kappa+1) geI[kappa,Ee][r] G[r])),{r,rinit,NumBR BohrRadius}]];
@@ -29,23 +32,28 @@ S4[k_?NumericQ,kappa_?NumericQ,J_?NumericQ,Ee_?NumericQ]:=Module[{jk=Abs[kappa]-
 lk=jk+1/2 kappa/Abs[kappa];1/2*NIntegrate[r^2 SphericalBesselJ[J,k r] ((1+(-1)^(lk+J)) (geI[kappa,Ee][r] G[r]+feI[kappa,Ee][r] F[r])+I (1-(-1)^(lk+J)) (feI[kappa,Ee][r] G[r]-geI[kappa,Ee][r] F[r])),{r,rinit,NumBR BohrRadius}]];
 NEe[Ee_,J_,kappa_]:=Module[{jk=Abs[kappa]-1/2,lk},
 lk=jk+1/2 kappa/Abs[kappa];
-16/(\[Pi] m^5)*Ee^2 (2 jk+1)*NIntegrate[kk^2 (((Efm-Ee)^2-kk^2) ((Abs[S0[kk,kappa,J,Ee] ]^2)/(J (J+1))+(Abs[S1[kk,kappa,J,Ee] ]^2)/((J+1) (2 J+1))+(Abs[Sm1[kk,kappa,J,Ee]]^2 )/(J (2 J+1)))+kk^2 (Abs[S4[kk,kappa,J,Ee]]^2+1/(2 J+1)^2 (S1[kk,kappa,J,Ee]+Sm1[kk,kappa,J,Ee]) Conjugate[(S1[kk,kappa,J,Ee]+Sm1[kk,kappa,J,Ee])])+(Efm-Ee) kk (1/(2 J+1) ((Conjugate[S1[kk,kappa,J,Ee]]+Conjugate[Sm1[kk,kappa,J,Ee]]) S4[kk,kappa,J,Ee]+Conjugate[(Conjugate[S1[kk,kappa,J,Ee]]+Conjugate[Sm1[kk,kappa,J,Ee]]) S4[kk,kappa,J,Ee]]))),{kk,10^-8,Efm-Ee}]]
+16/(\[Pi] m^5)*Ee^2 (2 jk+1)*NIntegrate[kk^2 (((Efm-Ee)^2-kk^2) ((Abs[S0[kk,kappa,J,Ee] ]^2)/(J (J+1))+(Abs[S1[kk,kappa,J,Ee] ]^2)/((J+1) (2 J+1))+(Abs[Sm1[kk,kappa,J,Ee]]^2 )/(J (2 J+1)))+kk^2 (Abs[S4[kk,kappa,J,Ee]]^2+1/(2 J+1)^2 (S1[kk,kappa,J,Ee]+Sm1[kk,kappa,J,Ee]) Conjugate[(S1[kk,kappa,J,Ee]+Sm1[kk,kappa,J,Ee])])+(Efm-Ee) kk (1/(2 J+1) ((Conjugate[S1[kk,kappa,J,Ee]]+Conjugate[Sm1[kk,kappa,J,Ee]]) S4[kk,kappa,J,Ee]+Conjugate[(Conjugate[S1[kk,kappa,J,Ee]]+Conjugate[Sm1[kk,kappa,J,Ee]]) S4[kk,kappa,J,Ee]]))),{kk,10^-8,Efm-Ee}]];
 
 NEeJ0[Ee_,J_,kappa_]:=Module[{jk=Abs[kappa]-1/2,lk},
-lk=jk+1/2 kappa/Abs[kappa];16/(\[Pi] m^5)*Ee^2 (2 jk+1)*NIntegrate[kk^2*(((Efm-Ee)^2-kk^2)*((Abs[S1[kk,kappa,J,Ee] ]^2)/((J+1) (2 J+1)))+kk^2 (Abs[S4[kk,kappa,J,Ee]]^2+1/(2 J+1)^2 Abs[S1[kk,kappa,J,Ee]]^2)+(Efm-Ee) kk (1/(2 J+1) ((Conjugate[S1[kk,kappa,J,Ee]]) S4[kk,kappa,J,Ee]+Conjugate[(Conjugate[S1[kk,kappa,J,Ee]]) S4[kk,kappa,J,Ee]]))),{kk,0,Efm-Ee}]]
+lk=jk+1/2 kappa/Abs[kappa];
+16/(\[Pi] m^5)*Ee^2 (2 jk+1)*NIntegrate[kk^2*(((Efm-Ee)^2-kk^2)*((Abs[S1[kk,kappa,J,Ee] ]^2)/((J+1) (2 J+1)))+kk^2 (Abs[S4[kk,kappa,J,Ee]]^2+1/(2 J+1)^2 Abs[S1[kk,kappa,J,Ee]]^2)+(Efm-Ee) kk (1/(2 J+1) ((Conjugate[S1[kk,kappa,J,Ee]]) S4[kk,kappa,J,Ee]+Conjugate[(Conjugate[S1[kk,kappa,J,Ee]]) S4[kk,kappa,J,Ee]]))),{kk,0,Efm-Ee}]];
 
 
 
 Emin=10^-5;
 Emax=Efm;
-Estep =0.005;
+Estep =0.1;
 Jmin=1;
 Jmax=31;
-name="Zal.1_j";
+name="test_Zal.1_j";
 dd=".dat";
+Print["J=0"];
 specJ0=ParallelTable[{Ee,1/(4 Ee^2)NEeJ0[Ee,0,-1] },{Ee,Emin,Emax,Estep}];
-Export["Zal0.1_j0.dat",specJ0]
-Do[specJ=ParallelTable[{Ee,1/(4 Ee^2)NEe[Ee,J,kappa] },{Ee,Emin,Emax,Estep}];
-Export[name<>ToString[J]<>"_k"<>ToString[kappa]<>dd,specJ];,{J,Jmin,Jmax,1},{kappa,-(J+1),-J,1}];
+Export["Zal0.1_j0.dat",specJ0];
+Do[Print["J=",J];
+Print["kappa=",kappa];
+specJ=ParallelTable[{Ee,1/(4 Ee^2)NEe[Ee,J,kappa] },{Ee,Emin,Emax,Estep}];
+Export[name<>ToString[J]<>"_k"<>ToString[kappa]<>dd,specJ];
+,{J,Jmin,Jmax,1},{kappa,-(J+1),-J,1}];
 
 
